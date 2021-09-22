7314f8c12290c28bb6e6b073f5fde0dd7f1c42ba
DESKTOP-Q0L5RKC\maria <maria.tirindelli@tum.de>
2021-09-22 10:20:05 +0200

    Adding script and example

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# shortest_path
+# Method Description
+
+The script expands an input network according to the Kirby and Potts for the calculation of the shortest path via the
+Dijkstra method. 
+
+The method reads a .csv file containing information on the network edges. The .csv file contains the
+following columns: 
+
+| from_stop_I  | to_stop_I  | d  |  n_vehicles |
+|--------------|------------|----|-------------|
+
+
+Where from_stop_I indicates the starting node, to_stop_I indicates the ending node, d the distance between nodes 
+(i.e. the weight of the edge), n_vehicles the vehicle number traveling on that edge.
+
+As an example: 
+
+| from_stop_I  | to_stop_I  | d  |  n_vehicles |
+|--------------|------------|----|-------------|
+|       1      |     4      | 10 |    black    |
+|       1      |     2      | 15 |    blue     |
+|       2      |     1      | 20 |    black    |
+|       2      |     1      | 15 |    blue     |
+|       2      |     3      | 5  |    blue     | 
+|       3      |     2      | 7  |    black    |
+|       3      |     2      | 5  |    blue     | 
+|       4      |     7      | 5  |    black    |
+|       4      |     5      | 15 |    red      |
+|       5      |     4      | 15 |    red      |
+|       5      |     9      | 18 |    red      |
+|       6      |     3      | 20 |    black    | 
+|       7      |     8      | 20 |    black    | 
+|       8      |     9      | 14 |    black    |
+|       9      |     5      | 18 |    red      | 
+|       9      |     6      | 16 |    black    | 
+
+
+Represents a network like: 
+
+![alt text](docs/network.png "Network")
+
+Any additional columns in the .csv file is ignored in the script. 
+
+The script "expand_network.py" expands the network using the Kirby and Potts expansion, as follows: 
+
+![alt text](docs/network_expanded.png "Network")
+
+This way, one can assign weights to the internal edges (in light green) to account for transfer delays occuring
+e.g. when changing bus. 
+
+The file transfer_delays.csv contains info on the transfer delays. The file contains the following columns: 
+
+| n_vehicles_in  | n_vehicles_in  | d  |  
+|----------------|----------------|----|
+
+As an example: 
+
+| n_vehicles_in  | n_vehicles_out | d  |  
+|----------------|----------------|----|
+|      black     |       blue     | 10 |
+|      blue      |       black    | 10 |
+|      black     |       red      | 5  |
+|      red       |       black    | 5  |
+|      blue      |       red      | 8  |
+|      red       |       blue     | 8  |
+
+Indicates that transition times to change between black and blue line is 10 (min), transition time between
+the black and red line is 5 (min) and transition time between the blue and red line is 8 min.
+If no transition time between certain lines combination is given, the script assign transition time = 0.
+
+! IMPORTANT NOTE: A given edge in the input network csv cannot contain multiple n_vehicles. That is, if more
+than one line is travelling from one node to the other there have to be multiple edges (rows) in the .csv file as well.
+As an example, both black and blue lines travel between node 1 and 2. Therefore, in the .csv files we have 2 lines:
+
+| from_stop_I  | to_stop_I  | d  |  n_vehicles |
+|--------------|------------|----|-------------|
+|     ...      |    ...     | .. |     ...     |
+|       1      |     2      | 15 |    blue     |
+|       2      |     1      | 20 |    black    |
+|     ...      |    ...     | .. |     ...     |
+
+# Usage
+
+The network edges have to be contained in the data/network.csv file
+The transition times between lines have to be contained in the data/transition_time.csv file
+
+An example network.csv and transition_time.csv are contained in the data folder. The script can be launched on the
+example as:
+```
+pip install -r requirements.txt
+python expand_network.py
+```
---
+++ expand_network.py
@@ -0,0 +1,157 @@
+import pandas as pd
+
+
+class Node:
+    def __init__(self, node_id, line):
+        self.node_id = node_id
+        self.line = line
+
+
+class Edge:
+    def __init__(self, starting_node_id, ending_node_id, distance, line):
+
+        self.starting_node_id = starting_node_id
+        self.ending_node_id = ending_node_id
+        self.distance = distance
+        self.line = line
+
+
+def get_nodes(pd_frame):
+    starting_nodes = pd_frame["from_stop_I"].tolist()
+    ending_nodes = pd_frame["to_stop_I"].tolist()
+    return set(starting_nodes + ending_nodes)
+
+
+def add_node(pd_frame, node):
+    for key in node.keys():
+        pd_frame[key].append(node[key])
+    return pd_frame
+
+
+def add_edge(network, edge):
+    network["from_stop_I"].append(edge.starting_node_id)
+    network["to_stop_I"].append(edge.ending_node_id)
+    network["d"].append(edge.distance)
+    network["n_vehicles"].append(edge.line)
+
+    return network
+
+
+def is_edge_in_network(network, edge):
+
+    common_sets = [i for i in range(0, len(network["from_stop_I"]))]
+
+    common_sets = [i for i in common_sets if str(network["from_stop_I"][i]) == str(edge.starting_node_id)]
+    common_sets = [i for i in common_sets if str(network["to_stop_I"][i]) == str(edge.ending_node_id)]
+    common_sets = [i for i in common_sets if str(network["d"][i]) == str(edge.distance)]
+    common_sets = [i for i in common_sets if str(network["n_vehicles"][i]) == str(edge.line)]
+
+    return len(common_sets) != 0
+
+
+def get_transfer_duration(pd_frame, line_in, line_out):
+
+    input_lines = pd_frame.loc[pd_frame["n_vehicles_in"] == line_in]
+
+    if len(input_lines) == 0:
+        return 0
+
+    selected_line = input_lines.loc[pd_frame["n_vehicles_out"] == line_out]
+
+    if len(selected_line) == 0:
+        return 0
+
+    duration = selected_line["d"]
+    assert len(duration) == 1
+    duration = duration[0]
+
+    return duration
+
+
+def main():
+
+    pd_frame = pd.read_csv("data/network.csv", delimiter=";")
+    transfer_time_frame = pd.read_csv("data/transition_time.csv", delimiter=";")
+    all_nodes = get_nodes(pd_frame)
+
+    extended_network = {"from_stop_I": [],
+                        "to_stop_I": [],
+                        "d": [],
+                        "n_vehicles": []}
+
+    for node in all_nodes:
+
+        incoming_nodes_idxs = pd_frame.loc[pd_frame["to_stop_I"] == node].index.tolist()
+        outgoing_connections_idxs = pd_frame.loc[pd_frame["from_stop_I"] == node].index.tolist()
+
+        new_in_nodes = []
+        new_out_nodes = []
+        for incoming_connections_idx in incoming_nodes_idxs:
+
+            line = str(pd_frame.loc[incoming_connections_idx, "n_vehicles"])
+
+            starting_node_id = str(pd_frame.loc[incoming_connections_idx, "from_stop_I"])
+            ending_node_id = str(node)
+            distance = pd_frame.loc[incoming_connections_idx, "d"]
+
+            starting_node = Node(node_id=starting_node_id + "_" + line + "_out",
+                                 line=line)
+
+            ending_node = Node(node_id = ending_node_id + "_" + line + "_in",
+                               line=line)
+
+            if ending_node.node_id not in [item.node_id for item in new_in_nodes]:
+                new_in_nodes.append(ending_node)
+
+            edge = Edge(starting_node_id=starting_node.node_id,
+                        ending_node_id=ending_node.node_id,
+                        distance=distance,
+                        line=line
+                        )
+
+            if not is_edge_in_network(extended_network, edge):
+                add_edge(extended_network, edge)
+
+        for outgoing_connections_idx in outgoing_connections_idxs:
+            line = str(pd_frame.loc[outgoing_connections_idx, "n_vehicles"])
+            distance = pd_frame.loc[outgoing_connections_idx, "d"]
+
+            starting_node_id = str(node) + "_" + line + "_out"
+            ending_node_id = str(pd_frame.loc[outgoing_connections_idx, "to_stop_I"]) + "_" + line + "_in"
+
+            starting_node = Node(node_id=starting_node_id,
+                                 line=line)
+
+            ending_node = Node(node_id=ending_node_id,
+                               line=line)
+
+            if starting_node.node_id not in [item.node_id for item in new_out_nodes]:
+                new_out_nodes.append(starting_node)
+
+            edge = Edge(starting_node_id=starting_node.node_id,
+                        ending_node_id=ending_node.node_id,
+                        distance=distance,
+                        line=line
+                        )
+
+            if not is_edge_in_network(extended_network, edge):
+                add_edge(extended_network, edge)
+
+        for node_in in new_in_nodes:
+            for node_out in new_out_nodes:
+
+                duration = get_transfer_duration(transfer_time_frame, line_in=node_in.line, line_out=node_out.line)
+
+                edge = Edge(starting_node_id = node_in.node_id,
+                            ending_node_id=node_out.node_id,
+                            distance=duration,
+                            line="transfer")
+
+                if not is_edge_in_network(extended_network, edge):
+                    add_edge(extended_network, edge)
+
+    expanded_frame = pd.DataFrame(extended_network)
+    expanded_frame.to_csv("data/expanded_network.csv")
+
+if __name__ == '__main__':
+    main()
